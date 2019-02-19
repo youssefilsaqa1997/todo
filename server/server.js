9ec5c7bbeddd614295d906164e37bd80da3e73c2
youssef ilsaqa <youssef_ilsaqa@hotmail.com>
2019-02-19 19:44:07 +0200

    =add api description

--- server/server.js
+++ server/server.js
@@ -15,50 +15,71 @@ var app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
-    res.send({massege:'welcome to todo server',
-    apis:[]
-          })
-})
-          
-app.post('/signUp', (req, res) => {
-    
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(req.body.password, salt, (err, hash) => {
-                var user = new User({
-                    email: req.body.email,
-                    password: hash,
-                    tasks: req.body.tasks,
-                    typeOfUser: req.body.typeOfUser
-                });
+app.get('/', (req, res) => {
+    res.send({
+        massege: 'welcome to todo server',
+        apis: [{
+            URL: "https://safe-chamber-93231.herokuapp.com/signUp",
+            Takes: {
+                email: "x@x.x",
+                password: "xxxxxx",
+                typeOfUser: "user",
+                tasks: []
+            }, gives: "object that added to DB"
+        },{
+            URL: "https://safe-chamber-93231.herokuapp.com/login",
+            Takes: {
+                email: "x@x.x",
+                password: "xxxxxx"
+            }, 
+            gives: {
+                email: "x@x.x",
+                typeOfUser: "user",
+                tasks: [{ name: "doing your job", statues: true }]
+            }
 
-                user.save().then((doc) => {
-                    res.send(doc);
-                }, (e) => {
-                    res.status(400).send(e);
-                })
+        }]
+    })
+})
+
+app.post('/signUp', (req, res) => {
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            var user = new User({
+                email: req.body.email,
+                password: hash,
+                tasks: req.body.tasks,
+                typeOfUser: req.body.typeOfUser
+            });
+
+            user.save().then((doc) => {
+                res.send(doc);
+            }, (e) => {
+                res.status(400).send(e);
             })
         })
+    })
 
 });
 
 
 
 app.post('/login', (req, res) => {
-    User.findOne({email:req.body.email}).then((userObject) => {
+    User.findOne({ email: req.body.email }).then((userObject) => {
         bcrypt.compare(req.body.password, userObject.password, (err, Res) => {
             if (Res == true) {
                 res.send({
-                    email:userObject.email,
-                    typeOfUser:userObject.typeOfUser,
-                    tasks:userObject.tasks
+                    email: userObject.email,
+                    typeOfUser: userObject.typeOfUser,
+                    tasks: userObject.tasks
                 })
-            }else{
-                res.status(400).send({error:"password don't match"})
+            } else {
+                res.status(400).send({ error: "password don't match" })
             }
         })
-    }).catch((e)=>{
-        res.status(404).send({error:"email not found"})
+    }).catch((e) => {
+        res.status(404).send({ error: "email not found" })
     })
 })
 
